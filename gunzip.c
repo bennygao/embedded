da326eceecd5d53fe2fbf2eeef826c2020308090
Benny Gao <bo.gao@hanshows.com>
2017-07-29 20:22:01 +0800

    解决bug，1. inflate中每次循环要把distance_bits, length_bits, literal_bits三个数组memset为全0；2. 动态huffman和静态huffman在一个else块里，而不是两个。

--- gunzip.c
+++ gunzip.c
@@ -4,57 +4,50 @@
 
 #include "gunzip.h"
 
-const static int LENGTH_EXTRA_BITS[] = {
-    0, 0, 0, 0, 0, 0, 0, 0,
-    1, 1, 1, 1, 2, 2, 2, 2,
-    3, 3, 3, 3, 4, 4, 4, 4,
-    5, 5, 5, 5, 0, 99, 99
-};
+const static int LENGTH_EXTRA_BITS[] = {0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 0, 99, 99};
 
-const static int LENGTH_VALUES[] = {
-    3, 4, 5, 6, 7, 8, 9, 10,
-    11, 13, 15, 17, 19, 23, 27, 31,
-    35, 43, 51, 59, 67, 83, 99, 115,
-    131, 163, 195, 227, 258, 0, 0
-};
+const static int LENGTH_VALUES[] = {3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 15, 17, 19, 23, 27, 31, 35, 43, 51, 59, 67, 83, 99, 115, 131, 163, 195, 227, 258, 0, 0};
 
-const static int DISTANCE_EXTRA_BITS[] = {
-    0, 0, 0, 0, 1, 1, 2, 2,
-    3, 3, 4, 4, 5, 5, 6, 6,
-    7, 7, 8, 8, 9, 9, 10, 10,
-    11, 11, 12, 12, 13, 13
-};
+const static int DISTANCE_EXTRA_BITS[] = {0, 0, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12, 13, 13};
 
-const static int DISTANCE_VALUES[] = {
-    1, 2, 3, 4, 5, 7, 9, 13,
-    17, 25, 33, 49, 65, 97, 129, 193,
-    257, 385, 513, 769, 1025, 1537, 2049, 3073,
-    4097, 6145, 8193, 12289, 16385, 24577
-};
+const static int DISTANCE_VALUES[] = {1, 2, 3, 4, 5, 7, 9, 13, 17, 25, 33, 49, 65, 97, 129, 193, 257, 385, 513, 769, 1025, 1537, 2049, 3073, 4097, 6145, 8193, 12289, 16385, 24577};
 
-const static int DYNAMIC_LENGTH_ORDER[] = {
-    16, 17, 18, 0, 8, 7, 9, 6,
-    10, 5, 11, 4, 12, 3, 13, 2, 14, 1, 15
-};
+const static int DYNAMIC_LENGTH_ORDER[] = {16, 17, 18, 0, 8, 7, 9, 6, 10, 5, 11, 4, 12, 3, 13, 2, 14, 1, 15};
 
 static void create_huffman_tree(byte *bits, int maxCode, int *tree) {
     int bl_count[MAX_BITS + 1];
     int code = 0, value = 0, left = 0, right = 0, node = 0;
     int next_code[MAX_BITS + 1];
-    int len = 0, i = 0, bit = 0;
+    int len = 0, i = 0, bit = 0, bv;
     int treeInsert = 1;
     
     memset(bl_count, 0, sizeof(bl_count));
     memset(next_code, 0, sizeof(next_code));
     
     for (i = 0; i < maxCode + 1; i++) {
-        bl_count[bits[i]]++;
+        bv = bits[i];
+        fprintf(stderr, "(%d %d)", bv, bl_count[bv]);
+        bl_count[bv]++;
     }
+    fprintf(stderr, "\n");
     bl_count[0] = 0;
     
-    for (i = 1; i <= MAX_BITS; i++) {
-        next_code[i] = code = (code + bl_count[i - 1]) << 1;
+    fprintf(stderr, "maxCode=%d bitsLength=%d bl_count[%d]: ", maxCode, maxCode + 1, MAX_BITS + 1);
+    for (i = 0; i < MAX_BITS + 1; ++i) {
+        fprintf(stderr, "%d ", bl_count[i]);
     }
+    fprintf(stderr, "\n");
+    
+    for (i = 1; i <= MAX_BITS; i++) {
+        bv = bl_count[i - 1];
+        code = (code + bv) << 1;
+        next_code[i] = code;
+    }
+    fprintf(stderr, "next_code[%d]: ", MAX_BITS + 1);
+    for (i = 0; i < MAX_BITS + 1; ++i) {
+        fprintf(stderr, "%d ", next_code[i]);
+    }
+    fprintf(stderr, "\n");
     
     for (i = 0; i <= maxCode; i++) {
         len = bits[i];
@@ -64,6 +57,7 @@ static void create_huffman_tree(byte *bits, int maxCode, int *tree) {
             for (bit = len - 1; bit >= 0; bit--) {
                 value = code & (1 << bit);
                 if (value == 0) {
+//                    fprintf(stderr, "node=%d\n", node);
                     left = (tree[node] >> 16) & 0xFFFF;
                     if (left == 0) {
                         tree[node] |= (treeInsert << 16);
@@ -82,6 +76,7 @@ static void create_huffman_tree(byte *bits, int maxCode, int *tree) {
                 }
             }
             
+//            fprintf(stderr, "i=%d node=%d\n", i, node);
             tree[node] = 0x80000000 | i;
         }
     }
@@ -157,10 +152,8 @@ byte* inflate(byte *compressed, int compressed_len, int *uncompressed_len) {
     int code = 0, leb = 0, deb = 0;
     int length = 0, distance = 0, offset = 0, save_index;
     struct inflate_context context;
+    int sequ = 0, bv;
     
-    memset(distance_bits, 0, sizeof(distance_bits));
-    memset(length_bits, 0, sizeof(length_bits));
-    memset(literal_bits, 0, sizeof(literal_bits));
     memset(&context, 0, sizeof(struct inflate_context));
     context.compressed = compressed;
     
@@ -201,103 +194,121 @@ byte* inflate(byte *compressed, int compressed_len, int *uncompressed_len) {
     btype = 0;
     
     do {
+        memset(distance_bits, 0, sizeof(distance_bits));
+        memset(length_bits, 0, sizeof(length_bits));
+        memset(literal_bits, 0, sizeof(literal_bits));
+        
         bfinal = read_bits(&context, 1);
         btype = read_bits(&context, 2);
-        if (bfinal == 0) {
-            context.error = ERR_MULTI_SEGMENTS;
-            return NULL;
-        }
         
         if (btype == BTYPE_NONE) { // 无压缩
+            fprintf(stderr, "--------\n");
             context.bit = 0;
             len = read_bits(&context, 16);
             read_bits(&context, 16);
             memcpy(uncompressed + uncompressed_index, compressed + context.index, len);
             context.index += len;
             uncompressed_index += len;
-        } else if (btype == BTYPE_DYNAMIC) { // 动态Huffman
-            hlit = read_bits(&context, 5) + 257;
-            if (hlit - 1 > MAX_CODE_LITERALS) {
-                context.error = ERR_LITERALS_NUM;
-                return NULL;
+        } else {
+            if (btype == BTYPE_DYNAMIC) { // 动态Huffman
+                fprintf(stderr, "********\n");
+                hlit = read_bits(&context, 5) + 257;
+                if (hlit - 1 > MAX_CODE_LITERALS) {
+                    context.error = ERR_LITERALS_NUM;
+                    return NULL;
+                }
+                
+                hdist = read_bits(&context, 5) + 1;
+                if (hdist - 1 > MAX_CODE_DISTANCES) {
+                    context.error = ERR_DISTANCES_NUM;
+                    return NULL;
+                }
+                
+                hclen = read_bits(&context, 4) + 4;
+                fprintf(stderr, "hclen=%d: ", hclen);
+                for (i = 0; i < hclen; i++) {
+                    bv = read_bits(&context, 3);
+                    length_bits[DYNAMIC_LENGTH_ORDER[i]] = (byte) bv;
+                    fprintf(stderr, "[%d=%d] ", DYNAMIC_LENGTH_ORDER[i], bv);
+                }
+                fprintf(stderr, "\n");
+                
+                context.lengths_num = MAX_CODE_LENGTHS;
+                //            fprintf(stderr, "treeSize=%ld\n", sizeof(context.lengths_tree) / sizeof(int));
+                memset(context.lengths_tree, 0, sizeof(context.lengths_tree));
+                create_huffman_tree(length_bits, MAX_CODE_LENGTHS, context.lengths_tree);
+                
+                context.literals_num = hlit - 1;
+                decode_code_lengths(&context, hlit, literal_bits);
+                //            fprintf(stderr, "treeSize=%ld\n", sizeof(context.literals_tree) / sizeof(int));
+                memset(context.literals_tree, 0, sizeof(context.literals_tree));
+                create_huffman_tree(literal_bits, hlit - 1, context.literals_tree);
+                
+                context.distances_num = hdist - 1;
+                decode_code_lengths(&context, hdist, distance_bits);
+                //            fprintf(stderr, "treeSize=%ld\n", sizeof(context.distances_tree) / sizeof(int));
+                memset(context.distances_tree, 0, sizeof(context.distances_tree));
+                create_huffman_tree(distance_bits, hdist - 1, context.distances_tree);
+            } else { // 静态Huffman
+                fprintf(stderr, "########\n");
+                for (int i = 0; i < 144; i++) {
+                    literal_bits[i] = 8;
+                }
+                
+                for (int i = 144; i < 256; i++) {
+                    literal_bits[i] = 9;
+                }
+                
+                for (int i = 256; i < 280; i++) {
+                    literal_bits[i] = 7;
+                }
+                
+                for (int i = 280; i < 288; i++) {
+                    literal_bits[i] = 8;
+                }
+                
+                create_huffman_tree(literal_bits, MAX_CODE_LITERALS, context.literals_tree);
+                
+                for (int i = 0; i < MAX_CODE_DISTANCES + 1; i++) {
+                    distance_bits[i] = 5;
+                }
+                
+                create_huffman_tree(distance_bits, MAX_CODE_DISTANCES, context.distances_tree);
             }
             
-            hdist = read_bits(&context, 5) + 1;
-            if (hdist - 1 > MAX_CODE_DISTANCES) {
-                context.error = ERR_DISTANCES_NUM;
-                return NULL;
+            code = leb = deb = 0;
+            sequ = 0;
+            while ((code = read_code(context.literals_tree, &context)) != EOB_CODE) {
+                if (code > EOB_CODE) {
+                    ++sequ;
+                    code -= 257;
+                    length = LENGTH_VALUES[code];
+                    if ((leb = LENGTH_EXTRA_BITS[code]) > 0) {
+                        length += read_bits(&context, leb);
+                    }
+                    
+                    code = read_code(context.distances_tree, &context);
+                    distance = DISTANCE_VALUES[code];
+                    if ((deb = DISTANCE_EXTRA_BITS[code]) > 0) {
+                        distance += read_bits(&context, deb);
+                    }
+                    
+                    offset = uncompressed_index - distance;
+                    while (distance < length) {
+                        memcpy(uncompressed + uncompressed_index, uncompressed + offset, distance);
+                        uncompressed_index += distance;
+                        length -= distance;
+                        distance <<= 1;
+                    }
+                    
+                    memcpy(uncompressed + uncompressed_index, uncompressed + offset, length);
+                    uncompressed_index += length;
+                } else {
+                    uncompressed[uncompressed_index++] = (byte) code;
+                }
             }
-            
-            hclen = read_bits(&context, 4) + 4;
-            for (i = 0; i < hclen; i++) {
-                length_bits[DYNAMIC_LENGTH_ORDER[i]] = (byte) read_bits(&context, 3);
-            }
-            
-            context.lengths_num = MAX_CODE_LENGTHS;
-            create_huffman_tree(length_bits, MAX_CODE_LENGTHS, context.lengths_tree);
-            
-            context.literals_num = hlit - 1;
-            decode_code_lengths(&context, hlit, literal_bits);
-            create_huffman_tree(literal_bits, hlit - 1, context.literals_tree);
-            
-            context.distances_num = hdist - 1;
-            decode_code_lengths(&context, hdist, distance_bits);
-            create_huffman_tree(distance_bits, hdist - 1, context.distances_tree);
-        } else { // 静态Huffman
-            for (int i = 0; i < 144; i++) {
-                literal_bits[i] = 8;
-            }
-            
-            for (int i = 144; i < 256; i++) {
-                literal_bits[i] = 9;
-            }
-            
-            for (int i = 256; i < 280; i++) {
-                literal_bits[i] = 7;
-            }
-            
-            for (int i = 280; i < 288; i++) {
-                literal_bits[i] = 8;
-            }
-            
-            create_huffman_tree(literal_bits, MAX_CODE_LITERALS, context.literals_tree);
-            
-            for (int i = 0; i < MAX_CODE_DISTANCES + 1; i++) {
-                distance_bits[i] = 5;
-            }
-            
-            create_huffman_tree(distance_bits, MAX_CODE_DISTANCES, context.distances_tree);
         }
-        
-        code = leb = deb = 0;
-        while ((code = read_code(context.literals_tree, &context)) != EOB_CODE) {
-            if (code > EOB_CODE) {
-                code -= 257;
-                length = LENGTH_VALUES[code];
-                if ((leb = LENGTH_EXTRA_BITS[code]) > 0) {
-                    length += read_bits(&context, leb);
-                }
-                
-                code = read_code(context.distances_tree, &context);
-                distance = DISTANCE_VALUES[code];
-                if ((deb = DISTANCE_EXTRA_BITS[code]) > 0) {
-                    distance += read_bits(&context, deb);
-                }
-                
-                offset = uncompressed_index - distance;
-                while (distance < length) {
-                    memcpy(uncompressed + uncompressed_index, uncompressed + offset, distance);
-                    uncompressed_index += distance;
-                    length -= distance;
-                    distance <<= 1;
-                }
-                
-                memcpy(uncompressed + uncompressed_index, uncompressed + offset, length);
-                uncompressed_index += length;
-            } else {
-                uncompressed[uncompressed_index++] = (byte) code;
-            }
-        }
+//        fprintf(stderr, "sequ=%d last_code=%d\n", sequ, code);
     } while (bfinal == 0);
     
     return uncompressed;
